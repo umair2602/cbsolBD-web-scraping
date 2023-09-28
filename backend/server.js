220349874bcd8feb0c4670667b3b9c946ffafb59
a-techie <azeemshafique2@gmail.com>
2023-09-29 01:51:37 +0500

    app

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,17 @@ const csv = require('csv-parser');
 const path = require('path');
 const Verifier = require('email-verifier');
 const axios = require('axios'); // Import axios for making HTTP requests
-const chromium = require('playwright-chromium'); // Import Playwright for web scraping
+const { chromium } = require('playwright');
 const ExcelJS = require('exceljs'); // Import ExcelJS for creating Excel files
-const validator = require('validator'); // Import validator for email validation
+const validator = require('email-validator'); // Import validator for email validation
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
+
+app.use(cors({
+  origin: '*'
+}))
 const port = 3001;
 
 const storage = multer.memoryStorage(); // Store files in memory
@@ -18,7 +24,7 @@ const upload = multer({ storage });
 app.use(express.json());
 
 const API_KEY = '0938863b743881ccd61ed300e9ca3dc9d774bdeb4686aa908faf7581bb5d9050'; // Replace with your SerpApi API key
-const EMAIL_VERIFIER_API_KEY ='at_lUQgplpwEGpBioomW7VSWwRVJau1k' // Replace with your Email Verifier API key
+const EMAIL_VERIFIER_API_KEY ='at_Lzsg1IJZ6NBVmqT0ICBo7a2gq01vA' // Replace with your Email Verifier API key
 const OUTPUT_FOLDER = 'output'; // Folder to save the results
 
 // Create an instance of the email verifier
@@ -47,41 +53,46 @@ async function processCsv(csvBuffer) {
       }
     }
 
-    async function extractEmailsFromLinks(serpData, queryFolder) {
-        const browser = await chromium.launch();
-        const context = await browser.newContext();
-
-      const emailAddresses = [];
-
-      for (const result of serpData) {
+    async function extractEmailsFromLinks(serpData) {
+      const browser = await chromium.launch();
+      const context = await browser.newContext();
+    
+      const emailAddresses = new Set(); // Use a Set to store unique email addresses
+    const limitedSerpData = serpData.slice(0,100)
+      for (const result of limitedSerpData) {
         try {
           const page = await context.newPage();
           await page.goto(result.link);
-
+    
           const emailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/g;
-
+    
           const pageContent = await page.content();
           const extractedEmails = pageContent.match(emailRegex);
-
+    
           if (extractedEmails) {
-            extractedEmails.forEach((email) => {
-              emailAddresses.push(email);
-            });
+            for (const email of extractedEmails) {
+             
+              emailAddresses.add(email); // Use a Set to automatically ensure uniqueness
+            }
           }
-
+    
           await page.close();
+    
+          // Check if the Set size exceeds 100 and return if true
+         
         } catch (error) {
           console.error(`Error visiting link '${result.link}': ${error.message}`);
         }
       }
-
-      const uniqueEmailAddresses = [...new Set(emailAddresses)];
-
+    
+      const uniqueEmailAddresses = Array.from(emailAddresses); // Convert Set to an Array
+    
       await context.close();
       await browser.close();
-
-      return uniqueEmailAddresses;
+    
+      return uniqueEmailAddresses.slice(0, 100); // Return up to 100 unique email addresses
     }
+    
 
     function filterEmailsByNames(emails, firstName, lastName, companyName) {
       const fullName = `${firstName} ${lastName}`;
@@ -112,15 +123,18 @@ async function processCsv(csvBuffer) {
     }
 
     async function verifyEmails(emails) {
-      const verificationResults = [];
-
+      const validEmails = [];
+    
       for (const email of emails) {
         const verificationResult = await verifyEmail(email);
-        verificationResults.push(verificationResult);
+        if (verificationResult.isValid) {
+          validEmails.push(email);
+        }
       }
-
-      return verificationResults;
+    
+      return validEmails;
     }
+    
 
     async function verifyEmail(email) {
       return new Promise((resolve, reject) => {
@@ -164,64 +178,110 @@ async function processCsv(csvBuffer) {
       }
     }
 
-    async function saveEmailsToExcel(emails, filePath, firstName, lastName, companyDomain, fullName) {
+  async  function saveEmailsToExcel(verificationResults, extractedEmails, filePath, firstName, lastName, companyDomain, fullName,verifierEmails) {
+    console.log("---------------------------------------------------------------------------------",
+      verificationResults, extractedEmails, filePath, firstName, lastName, companyDomain, fullName,
+      "-----------------------------------------------------------------------------------------------"
+    );
       const workbook = new ExcelJS.Workbook();
       const worksheet = workbook.addWorksheet('Extracted Emails');
-
+    
+      // Define columns for verifier findings
       worksheet.columns = [
         { header: 'Email', key: 'email', width: 40 },
-        { header: 'IsValid', key: 'isValid', width: 15 },
-        { header: 'Format Check', key: 'formatCheck', width: 15 },
-        { header: 'SMTP Check', key: 'smtpCheck', width: 15 },
-        { header: 'DNS Check', key: 'dnsCheck', width: 15 },
+        { header: 'Verifier', key: 'verifier', width: 40 },
+     
       ];
-
-      emails.forEach((result) => {
+    
+      // Add verifier findings to the worksheet
+      verificationResults.forEach((result) => {
         worksheet.addRow(result);
       });
-
+    
+      // Add a separator row between verifier findings and extracted emails
+      worksheet.addRow({});
+    
+      // Define columns for extracted emails
+      worksheet.columns = [
+        { header: 'Extracted-Email', key: 'email', width: 40 },
+        { header: 'Verifier', key: 'verifier', width: 40 },
+      ];
+    
+      // Add extracted emails to the worksheet
+      extractedEmails.forEach((email, verifier) => {
+        worksheet.addRow({ email, verifier });
+      });
+        // Add verifierEmails to the second column
+  verifierEmails.forEach((email) => {
+    worksheet.addRow({ email: '', verifier: email }); // Empty email in the 'email' column
+  });
       await workbook.xlsx.writeFile(filePath);
+      
+      return await filePath;
     }
+    
 
     async function processCsvField(csvBuffer) {
         console.log(csvBuffer, "here i am")
+        let excelFilePath='';
+        let extractedEmails = [];
+        let fullName ='';
+        let filteredEmails='';
+        let extractedEmailsVerification = '';
+        let firstName = '';
+        let lastName = '';
+        let companyName = '';
+        let companySite = '';
+        let companyDomain = extractDomain(companySite);
+        let fDomain;
+        let lDomain;
+        let flDomain;
+        let verifierEmails = [];
       try {
-        const csvData = csvBuffer.toString();
+        const csvData = csvBuffer.toString();//<-
         const rows = csvData.split('\n');
-
+console.log("rows ",rows)
         for (const row of rows) {
-          const columns = row.split(',');
 
+          const columns = row.split(',');
+console.log("columns ",columns)
           if (columns.length >= 4) {
-            const firstName = columns[0].trim();
-            const lastName = columns[1].trim();
-            const companyName = columns[2].trim();
-            const companySite = columns[3].trim();
-            const companyDomain = extractDomain(companySite);
+            firstName = columns[0].trim();
+            lastName = columns[1].trim();
+            companyName = columns[3].trim();
+            companySite = columns[4].trim();
+            companyDomain = extractDomain(companySite);
+            fDomain = `${firstName}@${companyDomain}`
+            lDomain = `${lastName}@${companyDomain}`
+            flDomain = `${firstName}.${lastName}@${companyDomain}`
+            verifierEmails.push(fDomain, lDomain, flDomain)
+            console.log("verifierEmails", verifierEmails)
+            
+          
 
             const query = `${firstName} ${lastName} ${companyName}`;
             const serpData = await fetchSerpData(query);
-
+            
+            // const queryFolder = path.join(OUTPUT_FOLDER, query);
+             excelFilePath= path.join(OUTPUT_FOLDER, 'filtered_emails.xlsx');
+            //  console.log("serpdata", serpData)
             if (serpData) {
-              const queryFolder = path.join(OUTPUT_FOLDER, query);
-
-              if (!fs.existsSync(queryFolder)) {
-                fs.mkdirSync(queryFolder);
-              }
-
-              const extractedEmails = await extractEmailsFromLinks(serpData, queryFolder);
-              const fullName = `${firstName} ${lastName}`;
-              const filteredEmails = filterEmailsByNames(extractedEmails, firstName, lastName, companyName);
-              const extractedEmailsVerification = await verifyEmails(filteredEmails);
-              const excelFilePath = path.join(queryFolder, 'filtered_emails.xlsx');
-              await saveEmailsToExcel(extractedEmailsVerification, excelFilePath, firstName, lastName, companyDomain, fullName);
-
+              extractedEmails = await extractEmailsFromLinks(serpData);
+              console.log("extracted emails", extractedEmails);
+              fullName = `${firstName} ${lastName}`;
+              filteredEmails = filterEmailsByNames(extractedEmails, firstName, lastName, companyName);
+              console.log(" filteredEmails ",filteredEmails)
+              // extractedEmailsVerification = await verifyEmails(filteredEmails);
+          
+              
               console.log(`Filtered and verified emails for ${fullName} saved in ${excelFilePath}`);
             }
           }
         }
+        file = await saveEmailsToExcel(filteredEmails, extractedEmails, excelFilePath, firstName, lastName, companyDomain, fullName, verifierEmails);
 
-        console.log(`All queries processed.`);
+        console.log(`All queries processed.`,excelFilePath);
+        console.log("file", file);
         return excelFilePath;
       } catch (error) {
         console.error('An error occurred during CSV processing:', error);
@@ -229,20 +289,29 @@ async function processCsv(csvBuffer) {
       }
     }
 
-    await processCsvField(csvBuffer);
+    // await processCsvField(csvBuffer).then((res)=>{
+    //   const processingResult = { message: 'CSV processing completed successfully' , path:res};
+    //   // Return the processing result
+    //   return processingResult;
+    // }).catch((err)=>{
+    //   console.log("here is error getting file path ", err)
+    //   const processingResult = { message: err , path:null};
+    //   return processingResult;
+    // });
 
-      
-    const processingResult = { message: 'CSV processing completed successfully' };
-
-    // Return the processing result
+    const processingResult = await processCsvField(csvBuffer);
+    console.log(" return processingResult; ",processingResult)
     return processingResult;
+    
   } catch (error) {
     console.error('An error occurred during CSV processing:', error);
     throw error; // You can choose to throw the error or handle it as needed
   }
 }
 
-app.post('/api/upload-csv', upload.single('csvFile'), async (req, res) => {
+
+app.post('/upload', upload.single('csvFile'), async (req, res) => {
+  console.log("req", req)
     try {
       if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
@@ -252,17 +321,11 @@ app.post('/api/upload-csv', upload.single('csvFile'), async (req, res) => {
       const csvBuffer = req.file.buffer;
   
       // Call the CSV processing function and pass the csvBuffer as an argument
-      const excelFilePath = await processCsv(csvBuffer);
-  
-      // Respond to the client with the processing result
-      res.download(excelFilePath, 'filtered_emails.xlsx', (err) => {
-        if (err) {
-          console.error('Error while sending file for download:', err);
-          res.status(500).json({ message: 'Error sending the file for download' });
-        } else {
-          console.log('Excel file sent for download');
-        }
-      });
+      const result = await processCsv(csvBuffer);
+      console.log("result is here hre here", result)
+    // Respond to the client with the processing result
+    res.json({ result }); 
+      
     } catch (error) {
       console.error('An error occurred:', error);
       res.status(500).json({ message: 'Error uploading and processing file' });
@@ -270,6 +333,26 @@ app.post('/api/upload-csv', upload.single('csvFile'), async (req, res) => {
   });
   
 
+  app.get('/download/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, 'output', filename); // Update the path as needed
+  
+    // Check if the file exists
+    if (fs.existsSync(filePath)) {
+      // Set the appropriate headers for the file download
+      res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  
+      // Send the file to the client
+      res.sendFile(filePath);
+    } else {
+      // If the file doesn't exist, return a 404 error
+      res.status(404).send('File not found');
+    }
+  });
+  
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
--- pages/home.js
+++ pages/home.js
@@ -1,28 +1,144 @@
 "use client";
-import React from 'react'
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
 
-const home = () => {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-      
-        window.location.href = '/'; // You can use Next.js's client-side routing like this
-    
-      };
+const FileUpload = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [downloadLink, setDownloadLink] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      // Handle the case where no file is selected
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("csvFile", selectedFile); // Make sure to use 'csvFile' as the field name
+
+    try {
+      const response = await fetch("http://localhost:3001/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        // Handle success (e.g., show a success message)
+        console.log("CSV file uploaded successfully");
+        const result = await response.json();
+        console.log(result, "result is here");
+        setDownloadLink(result.result);
+        console.log("result.result", result.result);
+      } else {
+        // Handle error (e.g., show an error message)
+        console.error("Failed to upload CSV file");
+      }
+    } catch (error) {
+      // Handle network or other errors
+      console.error("An error occurred while uploading the CSV file:", error);
+    }
+  };
+
+  const handleDownload = () => {
+    if (downloadLink) {
+      const filename = "filtered_emails.xlsx"; // Replace with the desired filename
+      const downloadUrl = `http://localhost:3001/download/${filename}`; // Replace with the correct endpoint
+
+      fetch(downloadUrl, {
+        method: "GET",
+      })
+        .then((response) => {
+          // Check if the response status is OK (200) and content type is valid
+          if (
+            response.status === 200 &&
+            response.headers.get("content-type") ===
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          ) {
+            // Create a blob from the response data
+            return response.blob();
+          } else {
+            // Handle the case where the file is not available or has an invalid content type
+            throw new Error("File not available for download");
+          }
+        })
+        .then((blob) => {
+          // Create a URL for the blob
+          const blobUrl = window.URL.createObjectURL(blob);
+
+          // Create an anchor element to trigger the download
+          const anchor = document.createElement("a");
+          anchor.href = blobUrl;
+          anchor.download = filename;
+          anchor.click();
+
+          // Clean up by revoking the blob URL
+          window.URL.revokeObjectURL(blobUrl);
+        })
+        .catch((error) => {
+          console.error("Download failed:", error.message);
+          // Handle the error (e.g., show an error message to the user)
+        });
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        onClick={handleUpload}
+      >
+        Upload
+      </Button>
+      {downloadLink && (
+        <>
+          <Button
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleDownload}
+          >
+            Download Processed File
+          </Button>
+          <a
+            id="downloadLink"
+            href={downloadLink}
+            download="filtered_emails.xlsx"
+            style={{ display: "none" }}
+          >
+            Download
+          </a>
+        </>
+      )}
+    </div>
+  );
+};
+
+const Home = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    window.location.href = "/"; // You can use Next.js's client-side routing like this
+  };
+
   return (
     <>
-    
-    <h1>HomePage</h1>
-    <Button
-              type="submit"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleSubmit}
-            >
-              Log Out
-            </Button>
+      <h1>HomePage</h1>
+      <Button
+        type="submit"
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        onClick={handleSubmit}
+      >
+        Log Out
+      </Button>
+      <FileUpload />
     </>
-  )
-}
+  );
+};
 
-export default home
+export default Home;
